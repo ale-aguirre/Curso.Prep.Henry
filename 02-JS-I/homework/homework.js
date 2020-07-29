5b7aba544583abd835ee21a4745e401b2238e7d3
ale-aguirre <ale.aguirre.2222@hotmail.com>
2020-07-29 18:44:27 -0300

    Homework IV

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -74,11 +74,7 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
  //return str1.length === str2.length;
- if (str1.length === str2.length ){
-  return true;
-  } else { 
-  return false;
-  }
+ return str1.length === str2.length;
 }
 
 function menosQueNoventa(num) {
